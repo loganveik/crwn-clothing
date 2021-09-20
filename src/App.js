8ae73e5f41fc9b06cf72d9a8d4f7df25781c1f8d
ljv29 <jumpswervelogan@gmail.com>
2021-09-20 16:02:44 -0700

    implemented firebase utils included ability to store authenticated users into firestore dsatabse

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/signIn-and-signUp/signIn-and-signUp';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -18,9 +18,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state)
+        })
+      }
+      this.setState({ currentUser: userAuth })
     });
   }
 
--- src/pages/signIn-and-signUp/signIn-and-signUp.jsx
+++ src/pages/signIn-and-signUp/signIn-and-signUp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './signIn-and-signUp.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
     <div className="signIn-and-signUp">
         <SignIn />
+        <SignUp />
     </div>
 )
 export default SignInAndSignUpPage;
